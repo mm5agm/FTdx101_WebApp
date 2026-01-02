8c57f352acd3ef0779daf54490734fa971f19d44
mm5agm <mm5agm@outlook.com>
2026-01-02 12:46:33 +0000

    Release v0.8.3: Improved frequency digit editing and UI robustness

--- Pages/Diagnostics.cshtml.cs
+++ Pages/Diagnostics.cshtml.cs
@@ -1,13 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FTdx101_WebApp.Pages
 {
-    public class PrivacyModel : PageModel
+    public class DiagnosticsModel : PageModel
     {
         public void OnGet()
         {
         }
     }
-
 }
---
+++ Pages/Ports.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO.Ports;
+using System.Collections.Generic;
+
+namespace FTdx101_WebApp.Pages
+{
+    public class PortsModel : PageModel
+    {
+        public List<string> AvailablePorts { get; private set; } = new();
+        public bool Com6Present { get; private set; }
+
+        public void OnGet()
+        {
+            AvailablePorts = new List<string>(SerialPort.GetPortNames());
+            Com6Present = AvailablePorts.Contains("COM6");
+        }
+    }
+}
