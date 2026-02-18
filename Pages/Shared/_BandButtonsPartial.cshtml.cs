11334411053d6750a5674e8fbe051543fc6767f8
mm5agm <mm5agm@outlook.com>
2026-02-18 12:29:47 +0000

    After deleting redundant files

--- Pages/Shared/_BandButtonsPartial.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace FTdx101_WebApp.Pages.Shared
-{
-    public class _BandButtonsPartialModel : PageModel
-    {
-        // No server-side logic required for this partial
-    }
-}
